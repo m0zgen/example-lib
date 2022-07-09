fd1a01e17473cbedc58f04b93e4341be52c4dee6
/:) <g.yevgeniy.p@gmail.com>
2022-07-09 13:46:39 +0200

    + first example - isRoot

---
+++ example.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Author: Yevgeniy Goncharov aka xck, http://sys-adm.in
+# Example lib-sh using: https://github.com/m0zgen/lib-sh
+
+# Envs
+# ---------------------------------------------------\
+PATH=$PATH:/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin
+SCRIPT_PATH=$(cd `dirname "${BASH_SOURCE[0]}"` && pwd)
+cd $SCRIPT_PATH
+
+# Libs / Configs
+# ---------------------------------------------------\
+source "$(pwd)/lib/lib.sh"
+# config="$(pwd)/config/config.json"
+
+
+# Checks
+# ---------------------------------------------------\
+isRoot
